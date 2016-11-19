9024f01fbd00ca7dde2127b240352f167018edfe
javierarilos <javier.arilos@gmail.com>
2016-11-19 10:25:14 +0100

    QuickCheck Heap implemented

--- week3-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week3-quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -30,18 +30,57 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   property("insert element into empty heap, when deleted its minimum, resulting heap should be empty") = forAll { (a: Int) =>
     val h = insert(a, empty)
-    deleteMin(h) == empty
+    isEmpty(deleteMin(h))
   }
 
   property("any heap, you should get a sorted sequence of elements when continually finding and deleting minima") = forAll { (h: H) =>
-    def allElemsLessThan(h: H, prevMin: Int): Boolean = {
-      val min = findMin(h)
-      if (min >= prevMin)
-        allElemsLessThan(deleteMin(h), min)
-      else
-        false
+    def allElemsMoreThan(h: H, prevMin: Int): Boolean = {
+      if (isEmpty(h)) true
+      else {
+        val currMin = findMin(h)
+        (currMin >= prevMin) && allElemsMoreThan(deleteMin(h), currMin)
+      }
     }
-    allElemsLessThan(h, Int.MaxValue)
+    allElemsMoreThan(h, Int.MinValue)
+  }
+
+  property("a minimum of the melding of any two heaps should return a minimum of one or the other") = forAll { (h1: H, h2: H) =>
+    findMin(meld(h1, h2)) == Math.min(findMin(h1), findMin(h2))
+  }
+
+  property("adding an elem to the empty, when deleting the min should return empty again") = forAll { (a: Int) =>
+    def h = insert(a, empty)
+
+    findMin(h) == a && deleteMin(h) == empty
+  }
+
+  property("melding empty and empty should return empty") = forAll { (a: Int) =>
+    meld(empty, empty) == empty
+  }
+
+  property("Recusivly finding and deleting elements in a Heap should return same elements") = forAll { (h: H) =>
+    def isSorted(h: H): Boolean =
+      if (isEmpty(h)) true
+      else {
+        val m = findMin(h)
+        val h2 = deleteMin(h)
+        isEmpty(h2) || (m <= findMin(h2) && isSorted(h2))
+      }
+    isSorted(h)
+  }
+
+  property("melding two heaps is equal to moving first min to second and removing it from min") = forAll { (h1: H, h2: H) =>
+    def heapEqual(h1: H, h2: H): Boolean =
+      if (isEmpty(h1) && isEmpty(h2)) true
+      else {
+        val m1 = findMin(h1)
+        val m2 = findMin(h2)
+        m1 == m2 && heapEqual(deleteMin(h1), deleteMin(h2))
+      }
+
+    val bothMelded = meld(h1, h2)
+    val meldedAfterMovingMin = meld(deleteMin(h1), insert(findMin(h1), h2))
+    heapEqual(bothMelded, meldedAfterMovingMin)
   }
 
 }
